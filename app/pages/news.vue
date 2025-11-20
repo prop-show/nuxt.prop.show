4da9bb08a7ff9e6898fb810693fcb94df5788ec3
whbbit1999 <luckyboy_dong@sina.com>
2025-11-20 08:04:11 +0800

    feat: 添加加载状态时的骨架屏

--- app/pages/news.vue
+++ app/pages/news.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import NewsLine from "~/components/NewsLine.vue"
 
-const { data: news } = useAsyncData(() => queryCollection("news").order("date", "DESC").all(), {
+const { data: news, pending } = useAsyncData(() => queryCollection("news").order("date", "DESC").all(), {
   default: () => [],
 })
 
@@ -15,12 +15,25 @@ useSeoMeta({
   <main>
     <PageHeader title="速报" description="一些库和工具的最新动态和更新消息" />
     <section className="mb-16"></section>
+
+    <section v-if="pending">
+      <UCard class="space-y-4 mb-4" v-for="i in 4" :key="i">
+        <template #header>
+          <USkeleton class="h-8 w-[250px]" />
+        </template>
+        <section class="space-y-2">
+          <USkeleton class="h-4" v-for="i in 3" :key="i" />
+        </section>
+      </UCard>
+    </section>
+
     <UEmpty
-      v-if="!news.length"
+      v-else-if="!news.length"
       icon="i-tabler-hourglass-empty"
       title="还没有东西哦"
       description="暂时没有新的速报内容。请稍后再来看看！"
     />
+
     <NewsLine :news v-else />
   </main>
 </template>
--- app/pages/videos.vue
+++ app/pages/videos.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-const { data: videos } = useAsyncData(() => queryCollection("videos").order("date", "DESC").all(), {
+const { data: videos, pending } = useAsyncData(() => queryCollection("videos").order("date", "DESC").all(), {
   default: () => [],
 })
 useSeoMeta({
@@ -12,12 +12,26 @@ useSeoMeta({
   <main>
     <PageHeader title="视频教程" description="通过高质量的视频内容，学习前端开发技能和最佳实践" />
     <section className="mb-16"></section>
+
+    <section v-if="pending" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <UCard class="group hover:shadow-lg transition-all" v-for="i in 6" :key="i">
+        <USkeleton class="h-auto w-full aspect-video mb-2" />
+
+        <USkeleton class="h-8 w-2/5 mb-2" />
+
+        <section class="space-y-2">
+          <USkeleton class="h-4 w-full" v-for="i in 2" :key="i" />
+        </section>
+      </UCard>
+    </section>
+
     <UEmpty
-      v-if="!videos.length"
+      v-else-if="!videos.length"
       icon="i-tabler-player-play"
       title="暂无视频"
       description="精彩内容即将上线，敬请期待！"
     />
+
     <VideoList :videos v-else />
   </main>
 </template>
